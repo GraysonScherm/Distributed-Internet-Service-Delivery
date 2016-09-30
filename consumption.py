ac64286e5262986590f51c7212b8ad82343f7fad
schermgr <schermgr@server-1.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-09-29 20:19:24 -0500

    Added consumption algorithim

--- consumption.py
+++ consumption.py
@@ -1,2 +1,13 @@
 #consumption function
 import sys
+
+serverPower = float(0)
+serverPowerIdle = float(sys.argv[1])
+serverPowerPeak = float(sys.argv[2])
+serverLoad = float(sys.argv[3])
+
+serverPower = serverPowerIdle + ((serverPowerPeak - serverPowerIdle) * serverLoad)
+
+
+
+

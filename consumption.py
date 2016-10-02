f0e60e7371b51e6eafb5c0db58d02046ce0af52f
schermgr <schermgr@server-1.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-02 18:12:45 -0500

    Better algorithim

--- consumption.py
+++ consumption.py
@@ -1,18 +1,18 @@
-#consumption function
-import sys
+from __future__ import division
 
 
-def main():
+def load(lambdaList, lambdaNominal, serverID):
 	serverPower = float(0)
 	serverPowerIdle = float(63)
 	serverPowerPeak = float(92)
 	#will need to alter serverLoad input later
-	serverLoad = float(sys.argv[1])
+	serverLoad = float((lambdaList[serverID]) / lambdaNominal)
 
 	serverPower = serverPowerIdle + ((serverPowerPeak - serverPowerIdle) * serverLoad)
-
+	print (serverLoad)
 	return serverPower
 
+load([1000, 2003, 1905], 5000, 1)
 
     
     

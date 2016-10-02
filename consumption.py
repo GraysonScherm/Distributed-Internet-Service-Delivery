022203650ab97584c678441db085aea716ca6ab0
schermgr <schermgr@server-1.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-02 16:10:42 -0500

    Added comment

--- consumption.py
+++ consumption.py
@@ -6,6 +6,7 @@ def main():
 	serverPower = float(0)
 	serverPowerIdle = float(63)
 	serverPowerPeak = float(92)
+	#will need to alter serverLoad input later
 	serverLoad = float(sys.argv[1])
 
 	serverPower = serverPowerIdle + ((serverPowerPeak - serverPowerIdle) * serverLoad)

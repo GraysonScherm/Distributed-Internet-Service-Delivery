17854049681c87cf9c4e383eac258bcd233c8178
grigorg <grigorg@controller.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-02 17:38:56 -0500

    change value to real type

--- server.py
+++ server.py
@@ -6,8 +6,12 @@ TCP_IP = '72.36.65.116'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024 #buffer size
 
-if len(sys.argv) < 2:
-  print ("Enter the server id")
+bs = 100
+interval = sys.argv[2]
+serverID = sys.argv[1]
+
+if len(sys.argv) < 3:
+  print ("Enter the server id and time interval")
   sys.exit(1)
 
 intf = 'eth1'
@@ -16,12 +20,12 @@ intf_ip = intf_ip[intf_ip.index('inet') + 1].split('/')[0]
 print intf_ip
 
 while True:
-  v = random.randint(1, 10)
+  gE = random.uniform(1,10)*bs
   ts = time.time()
-  MESSAGE = str(v) + ";" + sys.argv[1] + ";" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ";" + intf_ip
+  MESSAGE = str(gE) + ";" + serverID + ";" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ";" + intf_ip
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((TCP_IP, TCP_PORT))
   s.send(MESSAGE)
   s.close()
   print (MESSAGE)
-  time.sleep(60)
+  time.sleep(int(interval))

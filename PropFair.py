09f50523098b7692f1c7bb6af6d7fb653351c1d3
keivanB <keivan.bahmani@gmail.com>
2016-10-02 22:28:13 -0400

    Updated the values now it works

--- PropFair.py
+++ PropFair.py
@@ -1,6 +1,6 @@
-def Propfair(GEvector,Evector,T):
+def Propfair(GEvector,Evector,lambdaList,T):
     #green energy vector, Grid Energy vector , T is the previous scheduled memory
-    tc=5
+    tc=50
     NDC=len(GEvector)
     Metric=[0]*NDC    # Vector of the metric we used for scheduling
     for i in range(0,NDC):
@@ -9,10 +9,12 @@ def Propfair(GEvector,Evector,T):
     MAX=Metric.index(max(Metric)) #the index of the choosen one
     SClist=[0]*len(GEvector) #refresh the Schedule list
     SClist[MAX]=1  #The Data Center which is selected
-    for i in range(0,len(GEvector)):
+    for i in range(0,NDC):
+        lambdaList[i]=lambdaList[i]+SClist[i]
+    for i in range(0,NDC):
         if SClist[i]==1:
             T[i]=(1-(1/tc))*T[i]+((1/tc))*GEvector[i]
         else:
             T[i]=(1-(1/tc))*T[i]
-            
-    return SClist
+ 
+    return SClist, lambdaList

1e88aadaa99554ad56ca9c32866a9ffbf78e1e72
keivanB <keivan.bahmani@gmail.com>
2016-10-02 22:28:25 -0400

    Add files via upload

--- PropFairtest.py
+++ PropFairtest.py
@@ -1,5 +1,6 @@
-def Propfair(GEvector,Evector,T):
-    tc=5
+def Propfair(GEvector,Evector,lambdaList,T):
+    #green energy vector, Grid Energy vector , T is the previous scheduled memory
+    tc=50
     NDC=len(GEvector)
     Metric=[0]*NDC    # Vector of the metric we used for scheduling
     for i in range(0,NDC):
@@ -8,21 +9,26 @@ def Propfair(GEvector,Evector,T):
     MAX=Metric.index(max(Metric)) #the index of the choosen one
     SClist=[0]*len(GEvector) #refresh the Schedule list
     SClist[MAX]=1  #The Data Center which is selected
-    for i in range(0,len(GEvector)):
+    for i in range(0,NDC):
+        lambdaList[i]=lambdaList[i]+SClist[i]
+    for i in range(0,NDC):
         if SClist[i]==1:
             T[i]=(1-(1/tc))*T[i]+((1/tc))*GEvector[i]
         else:
             T[i]=(1-(1/tc))*T[i]
-        
+            
     print(T)
+    print(Metric)
     print(MAX)
     print(SClist)
-    return SClist
+    print(lambdaList)
+    print('----------------')
+    return SClist, lambdaList, Metric
 
-T=[1,1,1]
-GEvector=[2,1,1]
-Evector=[1]*3
-SClist=[0]*3
-
-for i in range(0,10):
- SClist=Propfair(GEvector,Evector,T)
+T=[1]*5
+GEvector=[20,5,2,1,1]
+Evector=[1]*5
+SClist=[0]*5
+lambdaList=[0]*5
+for i in range(0,500):
+ SClist,lambdaList, Metric=Propfair(GEvector,Evector,lambdaList,T)

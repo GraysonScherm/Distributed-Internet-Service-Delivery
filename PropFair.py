0026840292fa7ebf375ef2e5d218b307c0a78381
keivanB <keivan.bahmani@gmail.com>
2016-10-02 21:35:53 -0400

    Add files via upload

--- PropFair.py
+++ PropFair.py
@@ -1,10 +1,14 @@
 def Propfair(GEvector,Evector,T):
-    tc=5;
-    Dvector=[GEvector/T for GEvector,T in zip(GEvector,T)] #metric vector for decision making
-    MAX=Dvector.index(max(Dvector))
+    #green energy vector, Grid Energy vector , T is the previous scheduled memory
+    tc=5
+    NDC=len(GEvector)
+    Metric=[0]*NDC    # Vector of the metric we used for scheduling
+    for i in range(0,NDC):
+        Metric[i]=GEvector[i]/T[i]
+        
+    MAX=Metric.index(max(Metric)) #the index of the choosen one
     SClist=[0]*len(GEvector) #refresh the Schedule list
     SClist[MAX]=1  #The Data Center which is selected
-    print(SClist)
     for i in range(0,len(GEvector)):
         if SClist[i]==1:
             T[i]=(1-(1/tc))*T[i]+((1/tc))*GEvector[i]

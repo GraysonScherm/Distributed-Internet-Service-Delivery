2e9ba6614d1e61a403bdf8ea72cc6fd7835f2887
grigorg <grigorg@controller.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-03 09:32:23 -0500

    uncommited files

--- PropFairtest.py
+++ PropFairtest.py
@@ -1,25 +1,32 @@
 def Propfair(GEvector,Evector,T):
-    tc=5
+    tc = float(5)
     Dvector=[GEvector/T for GEvector,T in zip(GEvector,T)] #metric vector for decision making
+    T=list([1,1,1])
     MAX=Dvector.index(max(Dvector))
-    SClist=[0]*len(GEvector) #refresh the Schedule list
+    SClist=[0,0,0]#*len(GEvector) #refresh the Schedule list
     SClist[MAX]=1  #The Data Center which is selected
     print(SClist)
-    for i in range(0,len(GEvector)):
-        if SClist[i]==1:
-            T[i]=(1-(1/tc))*T[i]+((1/tc))*GEvector[i]
+    for i in range(0,3): #len(GEvector)):
+	if SClist[i]==1:
+  	  h=float(T[i])
+           # T[i]=((1-(1/tc))*T[i])+(((1/tc))*GEvector[i])
+	  h=float((1-(1/tc)))*float(h)	
         else:
             T[i]=(1-(1/tc))*T[i]
+
         
     print(T)
     print(MAX)
     print(SClist)
     return SClist
 
-T=[1,1,1]
-GEvector=[2,1,1]
+T=list([1,1,1])
+
 Evector=[1]*3
 SClist=[0]*3
+
+#print len(GEvector)
+GEvector=[2,1,1]
 
 for i in range(0,10):
  SClist=Propfair(GEvector,Evector,T)

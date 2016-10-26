13f8c7384037022cc8caec15927644e058279315
grigorg <grigorg@server-1.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-26 16:07:42 -0500

    Sendning the number of active users

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import commands
 import socket, sys
 import datetime
 import time, random
+import subprocess
+
 TCP_IP = '72.36.65.116'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024 #buffer size
@@ -22,7 +24,8 @@ print intf_ip
 while True:
   gE = random.uniform(1,10)*bs
   ts = time.time()
-  MESSAGE = str(gE) + ";" + serverID + ";" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ";" + intf_ip
+  numberOfActiveUsers = subprocess.call("sudo netstat -anp | grep :80 | grep ESTABLISHED | wc -l 2>&1", shell=True, stdout=subprocess.PIPE)
+  MESSAGE = str(gE) + ";" + serverID + ";" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ";" + intf_ip + ";" + str(numberOfActiveUsers)
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((TCP_IP, TCP_PORT))
   s.send(MESSAGE)

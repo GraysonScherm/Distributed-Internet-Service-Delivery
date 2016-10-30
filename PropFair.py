df00e52405967d3762115d16bfa401a9dfdb8404
grigorg <grigorg@controller.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-30 16:35:09 -0500

    Scheduler

--- PropFair.py
+++ PropFair.py
@@ -1,4 +1,9 @@
-def Propfair(GEvector,Evector,lambdaList,T):
+import os
+import sqlite3
+
+def Propfair(GEvector,Evector,lambdaList,T = 0):
+    if T == 0:
+		T = [1, 1, 1]
     #green energy vector, Grid Energy vector , T is the previous scheduled memory
     tc=50
     NDC=len(GEvector)
@@ -6,15 +11,42 @@ def Propfair(GEvector,Evector,lambdaList,T):
     for i in range(0,NDC):
         Metric[i]=GEvector[i]/T[i]
         
-    MAX=Metric.index(max(Metric)) #the index of the choosen one
+    MAX=Metric.index(max(Metric)) #the index of the choosen one  
+
     SClist=[0]*len(GEvector) #refresh the Schedule list
     SClist[MAX]=1  #The Data Center which is selected
-    for i in range(0,NDC):
-        lambdaList[i]=lambdaList[i]+SClist[i]
+    #for i in range(0,NDC):
+    #    lambdaList[i]=lambdaList[i]+SClist[i]
     for i in range(0,NDC):
         if SClist[i]==1:
             T[i]=(1-(1/tc))*T[i]+((1/tc))*GEvector[i]
         else:
             T[i]=(1-(1/tc))*T[i]
  
-    return SClist, lambdaList
+    #return SClist#, lambdaList
+    return MAX, T
+	
+def fetchServerInfo():
+	fd = os.open("/tmp/ryu/Distributed-Internet-Service-Delivery/controller.db", os.O_RDONLY)
+	conn = sqlite3.connect('/dev/fd/%d' % fd)
+	os.close(fd)
+	cursor = conn.cursor()
+	
+	currentEnergyValues = [0, 0, 0]
+	currentNumberOfUsers = [0, 0, 0]
+	
+	for i in range(0, 2):
+		cursor.execute("SELECT * from energyValues where id = (SELECT MAX(id) from energyValues where server  = ?)", str(i + 1))
+		fetchedData = cursor.fetchall()
+		currentEnergyValues[i] = fetchedData[0][1]
+		currentNumberOfUsers[i] = fetchedData[0][6]
+		
+	return currentEnergyValues, currentNumberOfUsers
+	
+GEvector, lambdaList = fetchServerInfo()
+#T = [1, 1, 1]
+MAX, T = Propfair(GEvector,0,lambdaList)
+print (MAX + 1)
+
+		
+	

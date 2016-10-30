d006520eff4b8936c7938fd301d39594598d8e02
grigorg <grigorg@controller.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-30 18:55:11 -0500

    Integration

---
+++ client_test.py
@@ -0,0 +1,19 @@
+import commands
+import socket, sys
+import datetime
+import time, random
+
+TCP_IP = 'server-1'
+TCP_PORT = 80
+
+BUFFER_SIZE = 1024
+
+
+while 1:
+ start_time = time.time()
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ s.connect((TCP_IP, TCP_PORT))
+ s.send('GET /file.bz2 /HTTP 1.1 \r\n\r\n')
+ data = (s.recv(1000000))
+ print "File received! Time: " + str(time.time() - start_time)
+ s.close()

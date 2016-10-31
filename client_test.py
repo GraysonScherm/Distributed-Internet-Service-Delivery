2eab57373d35b77a9566746101ff265e7fb8eda2
grigorg <grigorg@client-1.cs350-project-2.ch-geni-net.instageni.illinois.edu>
2016-10-31 11:03:43 -0500

    Adding unique port to each TCP session

--- client_test.py
+++ client_test.py
@@ -11,9 +11,11 @@ BUFFER_SIZE = 1024
 
 while 1:
  start_time = time.time()
- s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- s.connect((TCP_IP, TCP_PORT))
- s.send('GET /file.bz2 /HTTP 1.1 \r\n\r\n')
- data = (s.recv(1000000))
- print "File received! Time: " + str(time.time() - start_time)
- s.close()
+ for i in range (0,10):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   s.bind(('0.0.0.0', 9080 + i))
+   s.connect((TCP_IP, TCP_PORT))
+   s.send('GET /file.bz2 /HTTP 1.1 \r\n\r\n')
+   data = (s.recv(1000000))
+   print "File received! Time: " + str(time.time() - start_time)
+   s.close()
